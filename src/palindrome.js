da3969825ccd7e8a2fffe56079e849f4a01fcd22
Mariia Korzun <3151020m@gmail.com>
2020-11-17 16:17:25 +0200

    task3 is done

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,6 +1,18 @@
 const detectPalindrome = (str) => {
-    // Your implementation
-    // Read README.md file, if you not understand what to do
+    if (typeof str !== 'string') {
+        return 'Passed argument is not a string'
+    }
+    if (str.length === 0) { return 'String is empty' }
+    return isSubStringPolindrome(str);
+}
+
+function isSubStringPolindrome(string) {
+    let len = string.length
+    if (len <= 1) { return 'This string is palindrome!' }
+    if (string[0].toLowerCase() !== string[len - 1].toLowerCase()) {
+        return 'This string is not a palindrome!'
+    }
+    return isSubStringPolindrome(string.slice(1, len-1))
 };
 
 module.exports = detectPalindrome;
